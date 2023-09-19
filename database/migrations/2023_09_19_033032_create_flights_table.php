27b09dc50e103f830280fb3842abff64e79475de
Ydaehan <daehan821@naver.com>
2023-09-19 12:52:08 +0900

    docs : react 수업 '23.09.19

---
+++ database/migrations/2023_09_19_033032_create_flights_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            // int 타입의 auto_increment primary key id 칼럼 생성
+            $table->id();
+            $table->string('name');
+            $table->string('airline');
+            // datetime 데이터 타입으로 created_at, updated_at 이라는 두 개의 칼럼을 생성
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flights');
+    }
+};
