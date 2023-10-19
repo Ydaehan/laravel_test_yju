ce76feecafa1e209ff00107d6289bc96f2f4fb61
Ydaehan <daehan821@naver.com>
2023-10-19 12:56:21 +0900

    laravel 수업 '23.10.19

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,8 +13,9 @@ class Post extends Model
     // 대량 할당을 사용할 때, 개발자가 허용하는 컬럼 값만 취해서 레코드로 생성하기 위해서
     // 예를 들어, request에 created_at, updated_at 값이 있어도 그에 해당하는
     // 컬럼이 posts 테이블에 존재하지만, 그 값은 취하지 않고 레코드를 생성한다.
-    protected $fillable = [
+    // protected $fillable = ['title', 'content', 'user_id']; // 화이트 리스트
+    protected $guarded = [
         'content',
         'user_id',
-    ];
+    ]; // 블랙 리스트
 }
